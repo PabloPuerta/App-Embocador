2391bf15bfad16b9a0a9a860d4c0ce1814a76a32
Pablo <pablopuertacasado@gmail.com>
2024-12-25 22:00:28 +0100

    App con usuarios y fichajes de entrada y salida

---
+++ PruebaASPNETEmbocador - copia/Filters/SessionCheckAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaASPNETEmbocador.Filters
+{
+    public class SessionCheckAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // Verificar si hay un usuario con sesión iniciada
+            if (HttpContext.Current.Session["IdUsuario"] == null)
+            {
+                // Redirigir al login si no hay un usuario con sesión iniciada
+                filterContext.Result = new RedirectResult("~/Home/Index");
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
